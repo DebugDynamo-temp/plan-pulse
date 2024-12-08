c4be83f6927ea8516a0b5d0357df51fdba04af07
DebugDynamo-temp <chatk01@pfw.edu>
2024-12-08 17:23:19 -0500

    password validation corrected

--- server/planpulse/src/main/java/com/project/planpulse/validation/PasswordValidator.java
+++ server/planpulse/src/main/java/com/project/planpulse/validation/PasswordValidator.java
@@ -5,8 +5,7 @@ import jakarta.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
 
-    private static final String PASSWORD_PATTERN =
-            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&_\\-#^])[A-Za-z\\d@$!%*?&_\\-#^]{8,30}$";
 
     @Override
     public void initialize(ValidPassword constraintAnnotation) {
